0a605905b332e922ec460b93cc390b3dbe0875a5
freddieiboy <freddieiboy@gmail.com>
2016-01-16 14:28:26 -0800

    Quick test build.

---
+++ app/tests/crate.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import expect from 'expect';
+import Crate from '../components/Crate';
+
+describe('empty', () => {
+
+	it('should work', () => {
+		expect(true).toEqual(true);
+	})
+}) 
